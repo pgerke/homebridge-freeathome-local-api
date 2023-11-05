50dcc67e1dfd4805db5379c060788aa8642b2573
Philip Gerke <44918997+pgerke@users.noreply.github.com>
2023-11-05 22:22:30 +0100

    feat: Homebridge 1.7.0 support (#80)
    #79

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { API } from "homebridge";
 import { PLATFORM_NAME } from "./settings";
 import { FreeAtHomeHomebridgePlatform } from "./platform";
-import fetch from "node-fetch";
-globalThis.fetch = fetch;
 
 /** This method registers the platform with Homebridge */
 export = (api: API) => {
